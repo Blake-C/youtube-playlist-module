2e88fa2baa3eec5cb6e324e6c1ce1d42a45e91f4
Blake Cerecero <Blake@DigitalBlake.com>
2018-06-20 22:10:36 -0500

    [js, scss] Added base styles
    Signed-off-by: Blake Cerecero <Blake@DigitalBlake.com>

--- app/components/scripts/YoutubePlaylistModule.js
+++ app/components/scripts/YoutubePlaylistModule.js
@@ -47,24 +47,27 @@ export default class YoutubePlaylistModule {
 	_parse_data( response, element ) {
 		const wrapper      = document.createElement( 'div' );
 		const video_ul     = document.createElement( 'ul' );
+		const video_list   = document.createElement( 'div' );
 		const video_player = document.createElement( 'div' );
 
 		video_ul.className     = 'video-list';
 		video_player.className = 'video-player';
 		wrapper.className      = 'youtube-video-playlist';
+		video_list.className   = 'video-list-wrapper';
 
 		response.items.map( data => {
 			video_ul.innerHTML += `<li>
 				<a href="#" data-id="${ data.snippet.resourceId.videoId }">
-					${ data.snippet.title } <br />
-					<img src="${ data.snippet.thumbnails.medium.url }" /> <br />
+					<img src="${ data.snippet.thumbnails.medium.url }" />
+					<p>${ data.snippet.title }</p>
 				</a>
 			</li>`;
 		} );
 
-		video_player.innerHTML = '<iframe src="" frameborder="0" allowfullscreen></iframe>';
+		video_player.innerHTML = '<iframe src="" width="560" height="315" frameborder="0" allowfullscreen></iframe>';
+		video_list.appendChild( video_ul );
 		wrapper.appendChild( video_player );
-		wrapper.appendChild( video_ul );
+		wrapper.appendChild( video_list );
 		element.appendChild( wrapper );
 
 		const video_items = video_ul.getElementsByTagName( 'a' );
@@ -73,13 +76,16 @@ export default class YoutubePlaylistModule {
 			const iframe = video_player.childNodes[0];
 
 			// https://developers.google.com/youtube/player_parameters
-			const video_url = id => `http://www.youtube.com/embed/${ id }?${ this._param( this.additional_args ) }`;
+			const video_url = id => `http://www.youtube.com/embed/${ id }?${ this._param( this.iframe_options ) }`;
 
 			index === 0 ? iframe.setAttribute( 'src', video_url( item.getAttribute( 'data-id' ) ) ) : '';
+			index === 0 ? item.setAttribute( 'class', 'active' ) : '';
 
 			item.addEventListener( 'click', function( event ) {
 				event.preventDefault();
 
+				Array.from( video_items ).map( item => item.setAttribute( 'class', '' ) );
+				this.setAttribute( 'class', 'active' );
 				iframe.setAttribute( 'src', video_url( this.getAttribute( 'data-id' ) ) );
 			});
 		});
